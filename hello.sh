aba09c95f98b96b64ba95920d39535f8c4fd2a10
joshkenney <joshuagkenney@gmail.com>
2022-06-19 00:27:51 -0400

    refactored code in hello.sh

--- hello.sh
+++ hello.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # declare STRING variable
 STRING="Thanks for pooping."
+STRING1=" FARTS! "
 STRING2="You're welcome."
 # print variable on a screen
-echo $STRING$"FARTS"$STRING2
+echo $STRING$STRING1$STRING2
