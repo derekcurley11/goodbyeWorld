549a462a04dad3e92d210336e3ced415f1d49bca
joshkenney <joshuagkenney@gmail.com>
2022-06-19 22:32:46 -0400

    figured out bash arrays in hello.sh

--- hello.sh
+++ hello.sh
@@ -4,4 +4,19 @@ STRING="Thanks for pooping."
 STRING1=" FARTS! "
 STRING2="You're welcome."
 # print variable on a screen
-echo $STRING$STRING1$STRING2
+# echo $STRING$STRING1$STRING2
+
+# in bash, an array is a list of items not separated by commas
+myArray=($STRING $STRING1 $STRING2)
+
+# this prints one on each line
+for variable in "${myArray[*]}";
+    do
+        echo $variable
+    done
+
+# # see what happens when you use @ to index
+# for variable in "${myArray[@]}";
+# do
+#   echo $variable
+# done
