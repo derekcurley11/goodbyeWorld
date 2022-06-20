02847bc4076697f2fb6f42c5f88658c90617455b
joshkenney <joshuagkenney@gmail.com>
2022-06-20 12:55:26 -0400

    added list example to hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,25 @@
 '''
+Example of looping through an array of names in python
+
+run the script in the terminal by typing `python3 hello.py`
+'''
+
+Josh = 'Josh Kenney'
+Derek = 'Derek Curley'
+
+array = [Josh, Derek]
+
+for name in array:
+	print(name)
+
+
+'''
 Example of instantiating a Person class and two objects: Josh and Derek
 The Person class currently has two properties, firstName and lastName
 and one method, printFullName(), which prints the full name of the object
 
 See if you can practice adding properties (e.g. self.age)
-
-run the script in the terminal by typing `python3 hello.py`
 '''
-
 class Person:
 	def __init__(self, firstName, lastName):
 		self.firstName = firstName
