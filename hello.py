9f5650b651830dccfa4be6a1525a7cfa4bd2e20a
joshkenney <joshuagkenney@gmail.com>
2022-06-19 22:10:39 -0400

    added class example to hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,22 @@
-names=["I'm","Figuring","it","out"]
+'''
+Example of instantiating a Person class and two objects
+Josh and Derek
+The Person class currently has two properties, firstName and lastName
+and one method, printFullName(), which prints the full name of the object
 
-for i in names:
-	print(i)
+See if you can practice adding properties (e.g. self.age)
+'''
+
+class Person:
+	def __init__(self, firstName, lastName):
+		self.firstName = firstName
+		self.lastName = lastName
+	
+	def printFullName(self):
+		print(f"{self.firstName} {self.lastName}")
+
+Derek = Person("Derek", "Curley")
+Josh = Person("Josh", "Kenney")
+
+Derek.printFullName()
+Josh.printFullName()
